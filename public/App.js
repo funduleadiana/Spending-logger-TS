d393bb98d81194ba3ad03f7435bd709f3081affd
funduleadiana <diana.fundulea@yahoo.com>
2021-07-09 20:34:06 +0100

    selectors

--- public/App.js
+++ public/App.js
@@ -10,3 +10,6 @@ invoices.forEach(inv => {
 let form = document.querySelector('.new-item-form');
 //inputs
 const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
--- src/App.ts
+++ src/App.ts
@@ -18,3 +18,6 @@ let form = document.querySelector('.new-item-form') as HTMLFormElement;
 
 //inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
+const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
+const details = document.querySelector('#details') as HTMLInputElement;
+const amount = document.querySelector('#amount') as HTMLInputElement;

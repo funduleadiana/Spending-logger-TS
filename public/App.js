173c3d59098dd265cfd9336990acf1471e005756
funduleadiana <diana.fundulea@yahoo.com>
2021-07-12 11:57:13 +0100

    adding event listener to the form submit

--- public/App.js
+++ public/App.js
@@ -1,19 +1,12 @@
-const me = {
-    name: "Diana",
-    age: 30,
-    speak(text) {
-        console.log(text);
-    },
-    spend(amount) {
-        console.log('I spent', amount);
-        return amount;
-    }
-};
-const greetPerson = (person) => {
-    console.log('hello', person.name);
-};
-greetPerson(me);
 import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+let docOne;
+let docTwo;
+docOne = new Invoice('yoshi', 'web work', 120);
+docTwo = new Payment('Dan', 'work on school project', 50);
+let docs = [];
+docs.push(docOne);
+docs.push(docTwo);
 const invOne = new Invoice('mario', 'work On Website for Mario', 250);
 const invTwo = new Invoice('luigi', 'work on Luigi website', 300);
 let invoices = [];
@@ -28,3 +21,35 @@ const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
 const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
+});
+// interface isPerson {
+//     name: string; 
+//     age: number; 
+//     speak(a: string): void; //returns nothing -console.log()
+//     spend(a: number) : number;
+// }
+// const me : isPerson = {
+//     name: "Diana",
+//     age: 30,
+//     speak(text: string): void{
+//         console.log(text)
+//     },
+//     spend(amount: number) : number{
+//         console.log('I spent', amount);
+//         return amount;
+//     }
+// }
+// const greetPerson = (person: isPerson)=> {
+//     console.log('hello', person.name);
+// }
+// greetPerson(me);
--- src/App.ts
+++ src/App.ts
@@ -1,31 +1,18 @@
 
-interface isPerson {
-    name: string; 
-    age: number; 
-    speak(a: string): void; //returns nothing -console.log()
-    spend(a: number) : number;
-}
-
-const me : isPerson = {
-    name: "Diana",
-    age: 30,
-    speak(text: string): void{
-        console.log(text)
-    },
-    spend(amount: number) : number{
-        console.log('I spent', amount);
-        return amount;
-    }
-}
-
-const greetPerson = (person: isPerson)=> {
-    console.log('hello', person.name);
-}
-greetPerson(me);
-
 import { textSpanEnd } from "../../../node_modules/typescript/lib/typescript.js";
 import { Invoice } from "./classes/Invoice.js";
+import { Payment } from "./classes/Payment.js";
+import { HasFormatter } from "./interfaces/HasFormatter.js";
 
+let docOne: HasFormatter;
+let docTwo: HasFormatter;
+
+docOne = new Invoice('yoshi', 'web work', 120);
+docTwo = new Payment('Dan', 'work on school project', 50);
+
+let docs: HasFormatter[] = []
+docs.push(docOne);
+docs.push(docTwo);
 
 const invOne= new Invoice('mario', 'work On Website for Mario', 250);
 const invTwo = new Invoice('luigi', 'work on Luigi website', 300);
@@ -47,3 +34,40 @@ const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+
+
+form.addEventListener('submit', (e: Event)=> {
+    e.preventDefault();
+    let doc: HasFormatter;
+    if(type.value === 'invoice'){
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    } else{
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    } 
+    console.log(doc)
+})
+
+
+// interface isPerson {
+//     name: string; 
+//     age: number; 
+//     speak(a: string): void; //returns nothing -console.log()
+//     spend(a: number) : number;
+// }
+
+// const me : isPerson = {
+//     name: "Diana",
+//     age: 30,
+//     speak(text: string): void{
+//         console.log(text)
+//     },
+//     spend(amount: number) : number{
+//         console.log('I spent', amount);
+//         return amount;
+//     }
+// }
+
+// const greetPerson = (person: isPerson)=> {
+//     console.log('hello', person.name);
+// }
+// greetPerson(me);
